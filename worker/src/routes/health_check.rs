10a2f34704b0cd304ed50bc0c6ada3bb7ea12195
shafiquejamal <shafique.jamal@gmail.com>
2025-10-10 19:55:05 -0400

    Finish round-robin

--- worker/src/routes/health_check.rs
+++ worker/src/routes/health_check.rs
@@ -2,9 +2,15 @@ use std::time::Duration;
 
 use axum::{http::StatusCode, response::IntoResponse};
 
-use crate::utils::constants::HEALTH_CHECK_DELAY_MS;
+use crate::utils::constants::{HEALTH_CHECK_DELAY_MS, WORKER_NUMBER};
 
 pub async fn health_check() -> impl IntoResponse {
     std::thread::sleep(Duration::from_millis(*HEALTH_CHECK_DELAY_MS));
-    (StatusCode::OK, format!("delay: {}", *HEALTH_CHECK_DELAY_MS))
+    (
+        StatusCode::OK,
+        format!(
+            "worker: {}, delay: {}",
+            *WORKER_NUMBER, *HEALTH_CHECK_DELAY_MS
+        ),
+    )
 }
--- worker/src/utils/constants.rs
+++ worker/src/utils/constants.rs
@@ -5,6 +5,14 @@ use std::env as std_env;
 lazy_static! {
     pub static ref HEALTH_CHECK_DELAY_MS: u64 = set_health_check_delay_ms();
     pub static ref APP_ADDRESS: String = set_port();
+    pub static ref WORKER_NUMBER: &'static str = set_worker_number();
+}
+
+fn set_worker_number() -> &'static str {
+    dotenv().ok();
+    std_env::var(env::WORKER_NUMBER_VAR)
+        .unwrap_or_else(|_| "3000".to_string())
+        .leak()
 }
 
 fn set_port() -> String {
@@ -24,4 +32,5 @@ fn set_health_check_delay_ms() -> u64 {
 pub mod env {
     pub const HEALTH_CHECK_DELAY_MS_ENV_VAR: &str = "HEALTH_CHECK_DELAY_MS";
     pub const PORT_VAR: &str = "PORT";
+    pub const WORKER_NUMBER_VAR: &str = "WORKER_NUMBER";
 }
