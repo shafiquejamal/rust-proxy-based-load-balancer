6de7f8963114926a594427dad99c3c8ec4da01b4
shafiquejamal <shafique.jamal@gmail.com>
2025-10-10 20:30:33 -0400

    Use the same port internally for all workers

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,9 +1,8 @@
-use worker::Application;
-use worker::utils::APP_ADDRESS;
+use worker::{Application, utils::prod::APP_ADDRESS};
 
 #[tokio::main]
 async fn main() {
-    let app = Application::build(APP_ADDRESS.as_ref())
+    let app = Application::build(APP_ADDRESS)
         .await
         .expect("Failed to build app");
     app.run().await.expect("Failed to run app");
--- worker/src/utils/constants.rs
+++ worker/src/utils/constants.rs
@@ -4,7 +4,6 @@ use std::env as std_env;
 
 lazy_static! {
     pub static ref HEALTH_CHECK_DELAY_MS: u64 = set_health_check_delay_ms();
-    pub static ref APP_ADDRESS: String = set_port();
     pub static ref WORKER_NUMBER: &'static str = set_worker_number();
 }
 
@@ -13,12 +12,6 @@ fn set_worker_number() -> &'static str {
     std_env::var(env::WORKER_NUMBER_VAR)
         .unwrap_or_else(|_| "3000".to_string())
         .leak()
-}
-
-fn set_port() -> String {
-    dotenv().ok();
-    let port = std_env::var(env::PORT_VAR).unwrap_or_else(|_| "3000".to_string());
-    "0.0.0.0:".to_owned() + &port
 }
 
 fn set_health_check_delay_ms() -> u64 {
@@ -31,6 +24,9 @@ fn set_health_check_delay_ms() -> u64 {
 
 pub mod env {
     pub const HEALTH_CHECK_DELAY_MS_ENV_VAR: &str = "HEALTH_CHECK_DELAY_MS";
-    pub const PORT_VAR: &str = "PORT";
     pub const WORKER_NUMBER_VAR: &str = "WORKER_NUMBER";
+}
+
+pub mod prod {
+    pub const APP_ADDRESS: &str = "0.0.0.0:3000";
 }
