5b04ad0e58a844d93ba267ef0d1a9f48c6b6312d
shafiquejamal <shafique.jamal@gmail.com>
2025-10-11 23:11:31 -0400

    Finish implementing round-robin only strategy

---
+++ load_balancer/src/lib.rs
@@ -0,0 +1,58 @@
+use hyper::{client::ResponseFuture, Body, Client, Request, Uri};
+use std::str::FromStr;
+
+mod strategy;
+
+pub use strategy::RoundRobinStrategy;
+use strategy::Strategy;
+
+pub struct LoadBalancer {
+    client: Client<hyper::client::HttpConnector>,
+    strategy: Box<dyn Strategy>,
+}
+
+impl LoadBalancer {
+    pub fn new(mut strategy: Box<dyn Strategy>) -> Result<Self, String> {
+        if strategy.get_worker().is_none() {
+            return Err("No worker hosts provided".into());
+        }
+
+        Ok(LoadBalancer {
+            client: Client::new(),
+            strategy,
+        })
+    }
+
+    pub fn forward_request(&mut self, req: Request<Body>) -> ResponseFuture {
+        let mut worker_uri = self.get_worker().to_owned();
+
+        // Extract the path and query from the original request
+        if let Some(path_and_query) = req.uri().path_and_query() {
+            worker_uri.push_str(path_and_query.as_str());
+        }
+
+        // Create a new URI from the worker URI
+        let new_uri = Uri::from_str(&worker_uri).unwrap();
+
+        // Extract the headers from the original request
+        let headers = req.headers().clone();
+
+        // Clone the original request's headers and method
+        let mut new_req = Request::builder()
+            .method(req.method())
+            .uri(new_uri)
+            .body(req.into_body())
+            .expect("request builder");
+
+        // Copy headers from the original request
+        for (key, value) in headers.iter() {
+            new_req.headers_mut().insert(key, value.clone());
+        }
+
+        self.client.request(new_req)
+    }
+
+    fn get_worker(&mut self) -> &str {
+        self.strategy.get_worker().unwrap()
+    }
+}
--- load_balancer/src/main.rs
+++ load_balancer/src/main.rs
@@ -1,67 +1,11 @@
-use std::{convert::Infallible, net::SocketAddr, str::FromStr, sync::Arc};
+use std::{convert::Infallible, net::SocketAddr, sync::Arc};
 
 use hyper::{
-    client::ResponseFuture,
     service::{make_service_fn, service_fn},
-    Body, Client, Request, Response, Server, Uri,
+    Body, Request, Response, Server,
 };
+use load_balancer::{LoadBalancer, RoundRobinStrategy};
 use tokio::sync::RwLock;
-
-struct LoadBalancer {
-    client: Client<hyper::client::HttpConnector>,
-    worker_hosts: Vec<String>,
-    current_worker: usize,
-}
-
-impl LoadBalancer {
-    pub fn new(worker_hosts: Vec<String>) -> Result<Self, String> {
-        if worker_hosts.is_empty() {
-            return Err("No worker hosts provided".into());
-        }
-
-        Ok(LoadBalancer {
-            client: Client::new(),
-            worker_hosts,
-            current_worker: 0,
-        })
-    }
-
-    pub fn forward_request(&mut self, req: Request<Body>) -> ResponseFuture {
-        let mut worker_uri = self.get_worker().to_owned();
-
-        // Extract the path and query from the original request
-        if let Some(path_and_query) = req.uri().path_and_query() {
-            worker_uri.push_str(path_and_query.as_str());
-        }
-
-        // Create a new URI from the worker URI
-        let new_uri = Uri::from_str(&worker_uri).unwrap();
-
-        // Extract the headers from the original request
-        let headers = req.headers().clone();
-
-        // Clone the original request's headers and method
-        let mut new_req = Request::builder()
-            .method(req.method())
-            .uri(new_uri)
-            .body(req.into_body())
-            .expect("request builder");
-
-        // Copy headers from the original request
-        for (key, value) in headers.iter() {
-            new_req.headers_mut().insert(key, value.clone());
-        }
-
-        self.client.request(new_req)
-    }
-
-    fn get_worker(&mut self) -> &str {
-        // Use a round-robin strategy to select a worker
-        let worker = self.worker_hosts.get(self.current_worker).unwrap();
-        self.current_worker = (self.current_worker + 1) % self.worker_hosts.len();
-        worker
-    }
-}
 
 async fn handle(
     req: Request<Body>,
@@ -88,8 +32,9 @@ async fn main() {
         ]
     };
 
+    let round_robin_strategy = Box::new(RoundRobinStrategy::new(worker_hosts));
     let load_balancer = Arc::new(RwLock::new(
-        LoadBalancer::new(worker_hosts).expect("failed to create load balancer"),
+        LoadBalancer::new(round_robin_strategy).expect("failed to create load balancer"),
     ));
 
     let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 1337));
